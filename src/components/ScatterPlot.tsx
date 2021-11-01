1d55490acb9abae9cf00ca6532b87571a5b6c0e1
konafx <afxlieonon@gmail.com>
2021-11-01 18:03:55 +0900

    add children

--- src/components/ScatterPlot.tsx
+++ src/components/ScatterPlot.tsx
@@ -1,14 +1,19 @@
-import React, { SVGProps } from 'react';
+import React, { ReactNode, SVGProps } from 'react';
 
 interface ScatterPlotProps {
   width: number;
   height: number;
+  children?: ReactNode;
 }
 
 type Props = Omit<SVGProps<SVGSVGElement>, 'viewBox'> & ScatterPlotProps;
 
 const ScatterPlot: React.FC<Props> = (props) => {
-  const { width, height, ...others } = props;
+  const { width, height, children, ...others } = props;
 
-  return <svg width={width} height={height} version="1.1"></svg>;
+  return (
+    <svg width={width} height={height} version="1.1">
+      {children}
+    </svg>
+  );
 };
